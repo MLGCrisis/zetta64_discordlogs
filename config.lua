7e6465d7d3c33947cc062517910a3124ec23a8b3
GG79 <midlifegamingcrisis@gmail.com>
2022-12-31 09:33:33 +0200

    Added webhooks

--- config.lua
+++ config.lua
@@ -1,11 +1,11 @@
 Config = {}
 
-Config.whlink = '' -- Default Webhook Link
+Config.whlink = 'https://discord.com/api/webhooks/1058625473359384636/eg0PYKtZ0JWJznfqjJODe4sC_XMn-8tUcOIyJcWE9DThyXgPdy6vP4UdicpzXccMaorr' -- Default Webhook Link
 
 Config.loginLog = true -- Log when players join and leave?
-Config.loginLogLink = '' -- WebHook Link to log logis. (Only works when Confi.loginLog = true).
+Config.loginLogLink = 'https://discord.com/api/webhooks/1058625699612737596/RlhY1xkhmYaikOnItVt4TRWQYN8TUZGLpnprj5A6es2w57DcHoG0eb3r5m_lSopC-uug' -- WebHook Link to log logis. (Only works when Confi.loginLog = true).
 
 
-Config.whName = 'Zetta64 Logger' -- Name of the WebHook
+Config.whName = 'MLGC Logs' -- Name of the WebHook
 Config.whLogo = 'https://c.file.glass/HDgu1bZLqt.png' -- Avatar of the WebHook
 
